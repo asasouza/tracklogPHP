be96393f81380c5e3bf365ef9f1ec312546f0c45
Alex <asasouza.alex@gmail.com>
2017-10-08 20:21:50 -0300

    Ainda tentando

--- lib/tracklogPhp.main.php
+++ lib/tracklogPhp.main.php
@@ -1,14 +1,22 @@
 <?php
-function tracklog_autoloader($class_name){
-	if (file_exists($_SERVER["DOCUMENT_ROOT"].'/lib/'.$class_name . '.class.php')) {
-		require_once($_SERVER["DOCUMENT_ROOT"].'/lib/'.$class_name . '.class.php');
-	}elseif (file_exists($_SERVER["DOCUMENT_ROOT"].'/lib/formats/' . $class_name . '.class.php')) {
-		require_once($_SERVER["DOCUMENT_ROOT"].'/lib/formats/' . $class_name . '.class.php');
-	}elseif(file_exists($_SERVER["DOCUMENT_ROOT"].'/lib/exceptions/' . $class_name . '.class.php')){
-		require_once($_SERVER["DOCUMENT_ROOT"].'/lib/exceptions/' . $class_name . '.class.php');
-	}
-}
+// function tracklog_autoloader($class_name){
+	// if (file_exists($_SERVER["DOCUMENT_ROOT"].'/lib/'.$class_name . '.class.php')) {
+	// 	require_once($_SERVER["DOCUMENT_ROOT"].'/lib/'.$class_name . '.class.php');
+	// }elseif (file_exists($_SERVER["DOCUMENT_ROOT"].'/lib/formats/' . $class_name . '.class.php')) {
+	// 	require_once($_SERVER["DOCUMENT_ROOT"].'/lib/formats/' . $class_name . '.class.php');
+	// }elseif(file_exists($_SERVER["DOCUMENT_ROOT"].'/lib/exceptions/' . $class_name . '.class.php')){
+	// 	require_once($_SERVER["DOCUMENT_ROOT"].'/lib/exceptions/' . $class_name . '.class.php');
+	// }
+// }
 
-spl_autoload_register("tracklog_autoloader");
+// spl_autoload_register("tracklog_autoloader");
+require_once("lib/tracklog.class.php");
+require_once("lib/trackPoint.class.php");
+require_once("lib/formats/CSV.class.php");
+require_once("lib/formats/GeoJson.class.php");
+require_once("lib/formats/GPX.class.php");
+require_once("lib/formats/KML.class.php");
+require_once("lib/formats/TCX.class.php");
+require_once("lib/exceptions/tracklogPhpException.class.php");
 
 ?>

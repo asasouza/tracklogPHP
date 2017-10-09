07bfce88e584d531bd6a3526b47a2d370938c476
Alex <asasouza.alex@gmail.com>
2017-10-08 21:17:46 -0300

    Ainda testando.

--- tracklogPhpAjax.php
+++ tracklogPhpAjax.php
@@ -3,22 +3,21 @@ require_once("lib/tracklogPhp.main.php");
 if (isset($_FILES)) {
 	$extension = pathinfo($_FILES["tracklogFile"]["name"], PATHINFO_EXTENSION);
 	$file_path = $_FILES["tracklogFile"]["tmp_name"];
-	$response["file_path"] = $_FILES["tracklogFile"]["tmp_name"];
-	$response["file_content"] = file_get_contents($_FILES["tracklogFile"]["tmp_name"]);
-	// $tracklog = new $extension($file_path);
-	// $response["info_board"]["data_pace"] = $tracklog->getPace();
-	// $response["info_board"]["data_total_time"] = $tracklog->getTotalTime();
-	// $response["info_board"]["data_total_distance"] = $tracklog->getTotalDistance("kilometers");
-	// $response["info_board"]["data_elevation_gain"] = $tracklog->getElevationGain();
-	// $response["info_board"]["data_elevation_loss"] = $tracklog->getElevationLoss();
-	// $response["data_elevations"] = $tracklog->getElevations();
-	// $response["data_distances"] = $tracklog->getDistances();
+	
+	$tracklog = new $extension($file_path);
+	$response["info_board"]["data_pace"] = $tracklog->getPace();
+	$response["info_board"]["data_total_time"] = $tracklog->getTotalTime();
+	$response["info_board"]["data_total_distance"] = $tracklog->getTotalDistance("kilometers");
+	$response["info_board"]["data_elevation_gain"] = $tracklog->getElevationGain();
+	$response["info_board"]["data_elevation_loss"] = $tracklog->getElevationLoss();
+	$response["data_elevations"] = $tracklog->getElevations();
+	$response["data_distances"] = $tracklog->getDistances();
 
-	// $path = str_replace(" ", "", "tmp_files/".date("Y-m-d").$tracklog->getTrackName());
+	$path = str_replace(" ", "", "tmp_files/".date("Y-m-d").$tracklog->getTrackName());
 
-	// $tracklog->out("kml", $path);
+	$tracklog->out("kml", $path);
 
-	// $response["data_kml"] = $_SERVER["HTTP_HOST"]."/".$path;
+	$response["data_kml"] = $_SERVER["HTTP_HOST"]."/".$path;
 	
 	echo json_encode($response);
 }else{

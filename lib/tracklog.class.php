3ba0840b6f8eae6534a534f8c8fa8bdf9134b31a
Alex <asasouza.alex@gmail.com>
2018-01-16 21:30:06 -0200

    Implementado algoritmos suavização

--- lib/tracklog.class.php
+++ lib/tracklog.class.php
@@ -23,6 +23,105 @@ abstract class Tracklog {
 	public function error_handler($errno, $message){
 	}
 
+
+
+	//Inicio testes de suavização
+
+	public function movingAverage($array){
+		intval(sqrt(count($array))) % 2 != 0 ? $range = intval(sqrt(count($array))) : $range = intval(sqrt(count($array))) + 1;
+		$smoothedArray = [];
+		$rule = intval($range/2); //rule to get the values of closests elements in the array
+		for ($i=0; $i < count($array); $i++) { 
+			$sum = 0;
+			if ($i <= $rule) { //if these are the first values
+				for ($y=0; $y < $range; $y++) { 
+					$sum += $array[$y];
+				}
+				array_push($smoothedArray, $sum/$range);
+			}elseif ($i >= count($array)-$rule) { // if these are the last values
+				for ($y = count($array)-$range; $y < count($array) ; $y++) { 
+					$sum += $array[$y];
+				}
+				array_push($smoothedArray, $sum/$range);
+			}else{
+				for($y = ($i-$rule); $y <= ($i + $rule); $y++){
+					$sum += $array[$y];
+				}
+				array_push($smoothedArray, $sum/$range);
+			}
+		}
+		return $smoothedArray;	
+	}
+
+	public function lowPass($array){
+		$alpha = 0.6;
+		$smoothedArray = [];
+		array_push($smoothedArray, $array[0]);
+		for ($i=1; $i < count($array); $i++) { 
+			$smoothed = $smoothedArray[$i-1]+($array[$i]-$smoothedArray[$i-1])*$alpha;
+			array_push($smoothedArray, $smoothed);
+		}
+		return $smoothedArray;
+	}
+
+	public function qFilter($array){
+		$ufilter = $this->uFilter($array);
+		$lfilter = $this->lFilter($array);
+		$smoothedArray = [];
+		for ($i=0; $i < count($ufilter); $i++) { 
+			$smoothed = $ufilter[$i] + $lfilter[$i] - $array[$i];
+			array_push($smoothedArray, $smoothed);
+		}
+		return $smoothedArray;
+	}
+
+	public function uFilter($array){
+		$width = 1; //mudar width
+		$smoothedArray = [];
+
+		for ($i=0; $i < count($array); $i++) { 
+			if ($i < $width) { //if the firts values
+				array_push($smoothedArray, $array[$i]);
+			}	
+			elseif ($i >= count($array)-$width) { //if the lasts values
+				array_push($smoothedArray, $array[$i]);
+			}else{
+				//width 1
+				$smoothed = min( max($array[$i-1], $array[$i]), max($array[$i], $array[$i+1]));
+
+				//width 2
+				// $smoothed = min( max($array[$i-2], $array[$i-1], $array[$i]), max($array[$i-1], $array[$i], $array[$i+1]) ,max($array[$i], $array[$i+1], $array[$i+2]));
+				array_push($smoothedArray, $smoothed);
+			}
+		}
+		return $smoothedArray;
+	}
+
+	public function lFilter($array){
+		$width = 1; //mudar width
+		$smoothedArray = [];
+
+		for ($i=0; $i < count($array); $i++) { 
+			if ($i < $width) { //if the firts values
+				array_push($smoothedArray, $array[$i]);
+			}	
+			elseif ($i >= count($array)-$width) { //if the lasts values
+				array_push($smoothedArray, $array[$i]);
+			}else{
+				//width 1
+				$smoothed = max( min($array[$i-1], $array[$i]), min($array[$i], $array[$i+1]));
+
+				// width 2
+				// $smoothed = max( min($array[$i-2], $array[$i-1], $array[$i]), min($array[$i-1], $array[$i], $array[$i+1]) ,min($array[$i], $array[$i+1], $array[$i+2]));
+				array_push($smoothedArray, $smoothed);
+			}
+		}
+		return $smoothedArray;
+	}
+
+	// Fim testes de suavização
+
+
 	/**
 	* Populates the distance attribute of the TrackPoints objects in the $trackData array.
 	*/
--- tests.php
+++ tests.php
@@ -1,108 +1,24 @@
 <?php
 require_once('lib/tracklogPhp.main.php');
 
-$extensions = ['kml', 'tcx', 'gpx', 'csv', 'js'];
-$files = scandir("test_files/external_files_diversos");
-sort($files, SORT_NUMERIC);
-foreach ($files as $key => $file) {
-	$extension = pathinfo($file, PATHINFO_EXTENSION);
-	if (in_array($extension, $extensions)) {
-		$file_path = "test_files/external_files_diversos/".$file;
-		try {
-			($extension == "js") ? $extension = "geojson" : 0;
-			$time_start = microtime(true);
-			$tracklog = new $extension($file_path);
-			?>
-			<table border="1">
-				<tr style="text-align: center; font-weight:bolder;">
-					<td colspan="2"><?php echo $file?></td>
-				</tr>
-				<tr>
-					<td>Informação</td>
-					<td>Resultado</td>
-				</tr>
-				<tr>
-					<td>Pontos de GPS</td>
-					<td><?php echo count($tracklog->getPoints()) ?></td>
-				</tr>				
-				<tr>
-					<td>Distância Total</td>
-					<td><?php echo $tracklog->getTotalDistance("kilometers") ?></td>
-				</tr>				
-				<?php try {	?>
-						<tr>
-							<td>Tempo Total</td>
-							<td><?php echo $tracklog->getTotalTime() ?></td>
-						</tr>
-						<tr>
-							<td>Pace</td>
-							<td><?php echo $tracklog->getPace() ?></td>
-						</tr>
-						<tr>
-							<td>Velocidade Média</td>
-							<td><?php echo $tracklog->getAverageSpeed() ?></td>
-						</tr>
-					<?php } catch (Exception $e) { ?>
-						<tr>
-							<td>Tempo Total</td>
-							<td><?php echo $e->getMessage() ?></td>
-						</tr>
-						<tr>
-							<td>Pace</td>
-							<td><?php echo $e->getMessage() ?></td>
-						</tr>
-						<tr>
-							<td>Velocidade Média</td>
-							<td><?php echo $e->getMessage() ?></td>
-						</tr>
-					<?php } ?>
+$gpx = new GPX("test_files/gpx/test_correct.gpx");
 
-					<?php try { ?>
-						<tr>
-							<td>Elevação Máxima</td>
-							<td><?php echo $tracklog->getMaxElevation() ?></td>
-						</tr>
-						<tr>
-							<td>Subida Acumulada</td>
-							<td><?php echo $tracklog->getElevationGain() ?></td>
-						</tr>
-						<tr>
-							<td>Descida Acumulada</td>
-							<td><?php echo $tracklog->getElevationLoss() ?></td>
-						</tr>
-					<?php } catch (Exception $e) { ?>
-						<tr>
-							<td>Elevação Máxima</td>
-							<td><?php echo $e->getMessage() ?></td>
-						</tr>
-						<tr>
-							<td>Subida Acumulada</td>
-							<td><?php echo $e->getMessage() ?></td>
-						</tr>
-						<tr>
-							<td>Descida Acumulada</td>
-							<td><?php echo $e->getMessage() ?></td>
-						</tr>
-					<?php }?>
-					<tr>
-						<td>Tempo de Execução</td>
-						<td><?php  echo (microtime(true) - $time_start) ?></td>
-					</tr>
-			</table>
-			<?php } catch (Exception $e) { ?>
-				<table border="2">
-					<tr>
-						<td><?php echo $file ?></tr>
-						<td><?php echo $e->getMessage() ?></tr>
-					</tr>
-					<tr>
-						<td>Tempo de Execução</td>
-						<td><?php  echo (microtime(true) - $time_start) ?></td>
-					</tr>
-				</table>
+// $data = $gpx->getPaces("seconds");
 
-			<?php
-		}
-	}
+// $data = $gpx->movingAverage($gpx->getPaces("seconds"));
+
+// $data = $gpx->lowPass($gpx->getPaces("seconds"));
+
+$data = $gpx->qFilter($gpx->getPaces("seconds"));
+
+// $data = $gpx->qFilter([1,4,5,6,5,9,2,3,4,5]);
+
+
+
+foreach ($data as $key => $value) {
+	if ($key % 2 != 0) {
+		echo number_format($value,6,",","")."<br>";
+	}	
 }
+
 ?>

62702f1c45463dc1ffcb3fb966ba934bd20b887d
Alex <asasouza.alex@gmail.com>
2018-01-27 22:00:38 -0200

    Mudando leitura e escrita KML TRACKS

--- lib/formats/KML.class.php
+++ lib/formats/KML.class.php
@@ -20,27 +20,39 @@ class KML extends Tracklog{
 			$xml = simplexml_load_file($file);
 			$xml->registerXPathNamespace('kml', $xml->getNamespaces()[""]);
 			$xml->registerXPathNamespace('gx', 'http://www.google.com/kml/ext/2.2');
-			if (!empty($content = $xml->xpath('//kml:LineString/kml:coordinates')) && strlen($content[0]) > 0) {
-				foreach ($content as $linestring) {
-					$linestring = preg_split('/\s+/', trim($linestring));					
-					foreach ($linestring as $linestring) {
-						$pointData = explode(',', $linestring);	
-						$trackPoint = new TrackPoint();
-						$trackPoint->setLongitude($pointData[0]);
-						$trackPoint->setLatitude($pointData[1]);
-						isset($pointData[2]) ? $trackPoint->setElevation($pointData[2]) : 0;
-						array_push($this->trackData, $trackPoint);
-					}					
+			if (!empty($content = $xml->xpath('//kml:LineString')) && strlen($content[0]) > 0) {
+				foreach ($content as $trackSegment) {
+					$trackData = array();
+					foreach ($trackSegment->coordinates as $linestring) {
+						$linestring = preg_split('/\s+/', trim($linestring));
+						foreach ($linestring as $linestring) {
+							$pointData = explode(',', $linestring);	
+							$trackPoint = new TrackPoint();
+							$trackPoint->setLongitude($pointData[0]);
+							$trackPoint->setLatitude($pointData[1]);
+							isset($pointData[2]) ? $trackPoint->setElevation($pointData[2]) : 0;
+							array_push($trackData, $trackPoint);
+						}					
+					}
+					array_push($this->trackData, $trackData);
 				}
-			}elseif(!empty($times = $xml->xpath('//gx:Track/kml:when')) && !empty($points = $xml->xpath('//gx:Track/gx:coord')) && count($times) == count($points)){
-				foreach ($points as $i => $pointData) {
-					$pointData = explode(' ', $pointData);
-					$trackPoint = new TrackPoint();
-					$trackPoint->setLongitude($pointData[0]);
-					$trackPoint->setLatitude($pointData[1]);
-					isset($pointData[2]) ? $trackPoint->setElevation($pointData[2]) : 0; //verify if exists elevation data.
-					$trackPoint->setTime($times[$i]);
-					array_push($this->trackData, $trackPoint);
+			}elseif(!empty($content = $xml->xpath('//gx:Track'))  ){
+				foreach ($content as $trackSegment) {
+					$trackData = array();
+					if (!empty($times = $xml->xpath('//gx:Track/kml:when')) && !empty($points = $xml->xpath('//gx:Track/gx:coord')) && count($times) == count($points)) {
+						foreach ($points as $i => $pointData) {
+							$pointData = explode(' ', $pointData);
+							$trackPoint = new TrackPoint();
+							$trackPoint->setLongitude($pointData[0]);
+							$trackPoint->setLatitude($pointData[1]);
+							isset($pointData[2]) ? $trackPoint->setElevation($pointData[2]) : 0; //verify if exists elevation data.
+							$trackPoint->setTime($times[$i]);
+							array_push($trackData, $trackPoint);
+						}
+					}else{
+						throw new TracklogPhpException("This file doesn't appear to have any tracklog data.");
+					}
+					array_push($this->trackData, $trackData);
 				}
 			}else{
 				throw new TracklogPhpException("This file doesn't appear to have any tracklog data.");
@@ -48,7 +60,6 @@ class KML extends Tracklog{
 			$this->populateDistance();
 			isset($xml->xpath('//kml:Document/kml:name')[0]) ? $this->trackName = $xml->xpath('//kml:Document/kml:name')[0] : 0;			
 			return $this;
-
 		} catch (TracklogPhpException $e) {
 			throw $e;
 		}
@@ -67,41 +78,44 @@ class KML extends Tracklog{
 		$kml->addAttribute('xmlns:xmlns:xsi','http://www.w3.org/2001/XMLSchema-instance');
 		$kml->addAttribute('xmlns:xmlns:gx','http://www.google.com/kml/ext/2.2');
 		$kml->addAttribute('xsi:xsi:schemaLocation','http://www.opengis.net/kml/2.2 http://schemas.opengis.net/kml/2.2.0/ogckml22.xsd http://www.google.com/kml/ext/2.2 http://developers.google.com/kml/schema/kml22gx.xsd">');
-			$document = $kml->addChild('Document');
-				if ($this->hasTime()) {
-				$folder = $document->addChild('Folder');
-					if (isset($this->trackName)) {
-					$folder->addChild('name', $this->trackName);
-					}
-					$folder->addChild('open', 1);
-						$placemark = $folder->addChild('Placemark');
-							$gxtrack = $placemark->addChild('gx:gx:Track');
-								foreach ($this->trackData as $trackPoint) {
-									$gxtrack->addChild('when', $trackPoint->getTime());
-								}
-								foreach ($this->trackData as $trackPoint) {
-									$coordinates = $trackPoint->getLongitude().' '.$trackPoint->getLatitude();
-									$coordinates .= $this->hasElevation() ? ' '.$trackPoint->getElevation() : "";
-									$gxtrack->addChild('gx:gx:coord', $coordinates);
-								}
-				}else{
-				$placemark = $document->addChild('Placemark');
-					if (isset($this->trackName)) {
-						$placemark->addChild('name', $this->trackName);
-					}
-					$placemark->addChild('visibility', 1);
-					$placemark->addChild('open', 1);
-					$linestring = $placemark->addChild('LineString');
-						$linestring->addChild('extrude', 'true');
-						$linestring->addChild('tessellate', 'true');						
-						$trackData = '';
-						foreach ($this->trackData as $trackPoint) {
-							$trackData .= $trackPoint->getLongitude().','.$trackPoint->getLatitude();
-							$trackData .= $this->hasElevation() ? ',' . $trackPoint->getElevation().'&#10;' : '&#10;';
-						}
-						$coordinates = $linestring->addChild('coordinates', $trackData);							
+		$document = $kml->addChild('Document');
+		if ($this->hasTime()) {
+			$folder = $document->addChild('Folder');
+			if (isset($this->trackName)) {
+				$folder->addChild('name', $this->trackName);
+			}
+			$folder->addChild('open', 1);
+			$placemark = $folder->addChild('Placemark');
+			foreach ($this->trackData as $trackSegment) {
+				$gxtrack = $placemark->addChild('gx:gx:Track');
+				foreach ($trackSegment as $trackPoint) {
+					$gxtrack->addChild('when', $trackPoint->getTime());
 				}
-
+				foreach ($trackSegment as $trackPoint) {
+					$coordinates = $trackPoint->getLongitude().' '.$trackPoint->getLatitude();
+					$coordinates .= $this->hasElevation() ? ' '.$trackPoint->getElevation() : "";
+					$gxtrack->addChild('gx:gx:coord', $coordinates);
+				}
+			}			
+		}else{
+			$placemark = $document->addChild('Placemark');
+			if (isset($this->trackName)) {
+				$placemark->addChild('name', $this->trackName);
+			}
+			$placemark->addChild('visibility', 1);
+			$placemark->addChild('open', 1);
+			foreach ($this->trackData as $trackSegment) {
+				$linestring = $placemark->addChild('LineString');
+				$linestring->addChild('extrude', 'true');
+				$linestring->addChild('tessellate', 'true');						
+				$trackData = '';
+				foreach ($trackSegment as $trackPoint) {
+					$trackData .= $trackPoint->getLongitude().','.$trackPoint->getLatitude();
+					$trackData .= $this->hasElevation() ? ',' . $trackPoint->getElevation().'&#10;' : '&#10;';
+				}
+				$coordinates = $linestring->addChild('coordinates', $trackData);	
+			}
+		}
 		$dom = new DOMDocument('1.0', 'UTF-8');
 		$dom->preserveWhiteSpace = false;
 		$dom->formatOutput = true;
